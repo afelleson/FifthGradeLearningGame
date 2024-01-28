ce4b52b7a8c031ec6a4d4b9f601b9c00d42b9c24
afelleson <86610161+afelleson@users.noreply.github.com>
2024-01-28 13:45:29 -0600

    Rearrange error message and php and reformat
    - Move the error message into the html document properly so its visibility can be controlled. Move php directly below that so it appears when a php connection problem has occurred.
    - Fix indentation

--- GameDemo/menu.php
+++ GameDemo/menu.php
@@ -1,17 +1,6 @@
 <!-- Menu -->
 
-If this page is blank, there's probably a problem with the database. Either the login credentials are wrong, or an expected database, table, column, etc., is not being found.
-
-<?php
-//probably could be much shorter (just take part of the check_login function if it doesn't require a db connection)
-session_start();
-
-require_once('/etc/LearningGame/config.php');
-include("functions.php");
-
-$con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-$x = check_login($con);
-?>
+<!-- The php code is after </style> below -->
 
 <!DOCTYPE html>
 <html lang="en" >
@@ -23,84 +12,106 @@ $x = check_login($con);
 </head>
 <body>
 
-  <style>
-  *{
-      padding: 0;
-      margin: 0;
-      box-sizing: border-box;
-      font-family: 'Poppins', sans-serif;
-  }
-  body{
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      min-height: 100vh;
-      background: black;
-  }
-  ul{
-      position: relative;
-      display: flex;
-      flex-direction: column;
-      gap: 30px;
-  }
-  ul li{
-      position: relative;
-      list-style: none;
-  }
-  ul li a{
-      position: relative;
-      font-size: 4em;
-      text-decoration: none;
-      line-height: 1em;
-      letter-spacing: 2px;
-      text-transform: uppercase;
-      color: transparent;
-      -webkit-text-stroke: 1px rgba(255, 255, 255, 0.5);
-  }
-  ul li a::before{
-      content: attr(data-text);
-      position: absolute;
-      color: var(--clr);
-      width: 0;
-      overflow: hidden;
-      transition: 1s ;
-      border-right: 8px solid var(--clr);
-      -webkit-text-stroke: 1px var(--clr);
-  }
-  ul li a:hover::before{
-      width: 100%;
-      filter: drop-shadow(0 0 25px var(--clr));
-  }
-</style>
-<link rel="stylesheet" href="./starstyle.css">
-<!-- partial:index.partial.html -->
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif;
+        }
+        body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background: black;
+        }
+        ul{
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            gap: 30px;
+        }
+        ul li{
+            position: relative;
+            list-style: none;
+        }
+        ul li a{
+            position: relative;
+            font-size: 4em;
+            text-decoration: none;
+            line-height: 1em;
+            letter-spacing: 2px;
+            text-transform: uppercase;
+            color: transparent;
+            -webkit-text-stroke: 1px rgba(255, 255, 255, 0.5);
+        }
+        ul li a::before{
+            content: attr(data-text);
+            position: absolute;
+            color: var(--clr);
+            width: 0;
+            overflow: hidden;
+            transition: 1s ;
+            border-right: 8px solid var(--clr);
+            -webkit-text-stroke: 1px var(--clr);
+        }
+        ul li a:hover::before{
+            width: 100%;
+            filter: drop-shadow(0 0 25px var(--clr));
+        }
+    </style>
+    <link rel="stylesheet" href="./starstyle.css">
+    <!-- partial:index.partial.html -->
 
-<ul>
-            <li style="--clr:#00ade1">
-                <a href="./index.php" data-text="&nbsp;Play">&nbsp;Play&nbsp;</a>
-            </li>
-            <li style="--clr:#fcf86a">
-                <a href="./Leaderboard/leaderboard6.php" data-text="&nbsp;Leaderboard">&nbsp;Leaderboard&nbsp;</a>
-            </li>
-            <!-- <li style="--clr:#ffdd1c">
-                <a href="" data-text="&nbsp;Challenges">&nbsp;Challenges&nbsp;</a>
-            </li> -->
-            <li style="--clr:#ff6493">
-                <a href="https://forms.gle/LrFVFtCa8v3Rsn9j8" data-text="&nbsp;Report&nbsp;Bug">&nbsp;Report Bug&nbsp;</a>
-            </li>
-            <li style="--clr:#00dc82">
-                <a href="./logout.php" data-text="&nbsp;Logout">&nbsp;Logout&nbsp;</a>
-            </li>
-
-        </ul>
+    <div id="error-message">
+        If this page is blank, there's probably a problem with the database connection. Either the login credentials are wrong, or an expected database, table, column, etc., is not being found.
+    </div>
 
 
+    <?php
+        //probably could be much shorter (just take part of the check_login function if it doesn't require a db connection)
+        session_start();
+
+        include('/etc/LearningGame/config.php');
+        include("functions.php");
+
+        $con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        $x = check_login($con);
+    ?>
+
+
+    <style>
+        #error-message {
+            display: none;
+        }
+    </style>
+
+    <ul>
+        <li style="--clr:#00ade1">
+            <a href="./index.php" data-text="&nbsp;Play">&nbsp;Play&nbsp;</a>
+        </li>
+        <li style="--clr:#fcf86a">
+            <a href="./Leaderboard/leaderboard6.php" data-text="&nbsp;Leaderboard">&nbsp;Leaderboard&nbsp;</a>
+        </li>
+        <!-- <li style="--clr:#ffdd1c">
+            <a href="" data-text="&nbsp;Challenges">&nbsp;Challenges&nbsp;</a>
+        </li> -->
+        <li style="--clr:#ff6493">
+            <a href="https://forms.gle/LrFVFtCa8v3Rsn9j8" data-text="&nbsp;Report&nbsp;Bug">&nbsp;Report Bug&nbsp;</a>
+        </li>
+        <li style="--clr:#00dc82">
+            <a href="./logout.php" data-text="&nbsp;Logout">&nbsp;Logout&nbsp;</a>
+        </li>
+
+    </ul>
 
     <div id="stars"></div>
     <div id="stars2"></div>
     <div id="stars3"></div>
-<!-- partial -->
-  <script  src="./script.js"></script>
+    <!-- partial -->
+
+    <script  src="./script.js"></script>
 
 </body>
 </html>
